e2c26936957381c7811db859e7d08d7d5d0fc347
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-01-09 23:37:47 +0500

    add tarui api

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
+
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::command;
+
+// A simple command that can be called from the frontend
+#[command]
+fn greet(name: String) -> String {
+    format!("Hello, {}! 👋 From Rust", name)
+}
+
 fn main() {
-    hello_app_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
