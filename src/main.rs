8ac9f99af6ca712115d2d22aca27a83e0b777578
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-02-02 23:38:27 +0500

    this is new method for string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     println!("Hello, world!");
-    println!("this is new line")
+    println!("this is new line");
+
+    let _name = "waqas khan";
+    let _x = 20;
+    let firstname = String::from("new name ");
+    println!("my name is {} , {}",_name , firstname)
 }
 
